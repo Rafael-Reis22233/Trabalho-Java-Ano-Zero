ae680323c604aa9aa87adc32c5ee58205a2d8cfe
Diogo Coelho <70858461+DiogoCoelhoWeb@users.noreply.github.com>
2022-02-23 13:19:13 +0000

    Menu 23-02-2022
    Fiz o menu praticamente todo só falata adicionar as opções para trabalhar com as funcionalidades que vamos adicionar.
    
    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,59 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        menu();
+    String option;
 
+    boolean exit = false;
+
+    do {
+
+      option = menu();
+
+      switch (option){
+        case "Inserir Aluno":
+          System.out.println("INSERIR");
+          break;
+
+        case "Atualizar Informação":
+          System.out.println("ATUALIZAR");
+          break;
+
+        default:
+          exit = exitMenu();
+      }
+    } while (!exit);
+  }
+
+  public static String menu() {
+
+    String[] options = {"Inserir Aluno", "Atualizar Informação", "Sair"};
+
+    String selectedOption;
+
+    selectedOption = (String) JOptionPane.showInputDialog(null, "Selecione o que pretende fazer:\n\n", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+    return selectedOption;
+
+  }
+
+  private static boolean exitMenu() {
+
+    String[] options = {"Sim", "Não"};
+
+    JLabel lbl = new JLabel("Quer mesmo sair?");
+
+    JPanel panel = new JPanel();
+    panel.add(lbl);
+
+    int selectedOption = JOptionPane.showOptionDialog(null, panel, "The Title", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[1]);
+
+    if (selectedOption == 1){
+      return false;
     }
 
-    public static void menu(){
+    return true;
 
-
-
-    }
+  }
 }
