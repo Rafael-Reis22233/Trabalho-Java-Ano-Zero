14e8eb8c0bb655a28cf057d132d07bfb60d311be
Diogo Coelho <70858461+DiogoCoelhoWeb@users.noreply.github.com>
2022-02-23 22:17:02 +0000

    Error Handling e TODO
    Funções inserirAlunos() e verAlunos() juntamente com todas as funções associadas completas e com sistema para evitar erros na execução do programa
    
    23/02/2022 22:16

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+/*
+* Desenvolvido por: Grupo 4 "Os Grandes"
+* Versão: 1.0
+* Ultima Modificação: 23-02-2022 22:14
+* */
 import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
 
     //Constantes
-    final int TAMANHO = 1;
+    final int TAMANHO = 50;
 
     //Variaveis
     int nElems = 0;
@@ -37,10 +42,25 @@ public class Main {
 
         case "Atualizar Informação":
           System.out.println("ATUALIZAR");
+          //TODO: atualizarAluno()
           break;
 
         case "Ver Alunos":
-          verAlunos(turmas, numeros, nomes, algNotas, javaNotas, vbNotas, nElems);
+          if (nElems > 0){
+            verAlunos(turmas, numeros, nomes, algNotas, javaNotas, vbNotas, nElems);
+          }else {
+            JOptionPane.showMessageDialog(null, "Não existem alunos inseridos!", "Sem alunos!", JOptionPane.WARNING_MESSAGE);
+          }
+          break;
+
+        case "Carregar Alunos":
+          System.out.println("CARREHAR ALUNOS");
+          //TODO: carregarAlunos()
+          break;
+
+        case "Exportar Alunos":
+          System.out.println("EXPORTAR ALUNOS");
+          //TODO: exportarAlunos()
           break;
 
         default:
@@ -53,11 +73,20 @@ public class Main {
 
   private static int inserirAluno(String[] turmas, String[] nomes, int[] numeros, int[] algNotas, int[] javaNotas, int[] vbNotas, int nElems) {
 
-    nomes[nElems] = JOptionPane.showInputDialog("Insira o nome do(a) aluno(a):");
+    do {
+      nomes[nElems] = JOptionPane.showInputDialog(null, "Insira o nome do(a) aluno(a):", "Inserir Aluno",  JOptionPane.PLAIN_MESSAGE);
+
+      System.out.println(nomes[nElems].length());
+
+      if (nomes[nElems].length() == 0){
+        JOptionPane.showMessageDialog(null, "Por favor preencha o campo do nome!", "Nome inválido!", JOptionPane.WARNING_MESSAGE);
+      }
+
+    }while (nomes[nElems].length() == 0);
 
     do{
 
-      turmas[nElems] = JOptionPane.showInputDialog("Insira a turma do aluno(a) "+ nomes[nElems] +":");
+      turmas[nElems] = JOptionPane.showInputDialog(null, "Insira a turma do aluno(a) "+ nomes[nElems] +":", "Inserir Aluno", JOptionPane.PLAIN_MESSAGE).toUpperCase();
 
       if (turmas[nElems].length() != 1){
         JOptionPane.showMessageDialog(null, "A turma só pode ser constituida por uma letra!", "Turma inválida!", JOptionPane.WARNING_MESSAGE);
@@ -65,10 +94,10 @@ public class Main {
 
     }while (turmas[nElems].length() != 1);
 
-    numeros[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira o numero do(a) aluno(a) " + nomes[nElems] + ":"));
-    algNotas[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota de Algoritmia do(a) aluno(a) " + nomes[nElems] + ":"));
-    javaNotas[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota de Java do(a) aluno(a) " + nomes[nElems] + ":"));
-    vbNotas[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota de Visual Basic do(a) aluno(a) " + nomes[nElems] + ":"));
+    numeros[nElems] = checkNumber(JOptionPane.showInputDialog(null, "Insira o numero do(a) aluno(a) " + nomes[nElems] + ":", "Inserir Aluno", JOptionPane.PLAIN_MESSAGE), nomes[nElems]);
+    algNotas[nElems] = checkNota(JOptionPane.showInputDialog(null, "Insira a nota de Algoritmia do(a) aluno(a) " + nomes[nElems] + ":", "Inserir Aluno", JOptionPane.PLAIN_MESSAGE), nomes[nElems], "Algoritmia");
+    javaNotas[nElems] = checkNota(JOptionPane.showInputDialog(null, "Insira a nota de Java do(a) aluno(a) " + nomes[nElems] + ":", "Inserir Aluno",JOptionPane.PLAIN_MESSAGE), nomes[nElems], "Java");
+    vbNotas[nElems] = checkNota(JOptionPane.showInputDialog(null, "Insira a nota de Visual Basic do(a) aluno(a) " + nomes[nElems] + ":", "Inserir Aluno", JOptionPane.PLAIN_MESSAGE), nomes[nElems], "Visual Basic");
 
     nElems++;
 
@@ -169,7 +198,7 @@ public class Main {
   /* Menus e outras necessidades*/
   public static String menu() {
 
-    String[] options = {"Inserir Aluno", "Atualizar Informação", "Ver Alunos", "Sair"};
+    String[] options = {"Inserir Aluno", "Atualizar Informação", "Ver Alunos", "Carregar Alunos", "Exportar Alunos", "Sair"};
 
     String selectedOption;
 
@@ -202,7 +231,7 @@ public class Main {
 
     String tabela = "";
 
-    tabela = "<html> <head> <style> table { font-family: arial, sans-serif; border-collapse: collapse; width: 750px; } td, th { border: 1px solid #dddddd; text-align: left; padding: 5px; } tr:nth-child(even) { background-color: #dddddd; font-weight: bold} </style> </head> <body> <table> <tr> <th>Turma</th> <th>Número</th> <th>Nome</th> <th>Alg</th> <th>Java</th> <th>VB</th> <th>Final</th> </tr>";
+    tabela = "<html> <head> <style> table { font-family: arial, sans-serif; border-collapse: collapse; width: 500px; margin-right: 0,} td, th { border: 2px solid #dddddd; text-align: left; margin-right: 0;} tr:nth-child(even) { background-color: #000000; font-weight: bold} </style> </head> <body> <table> <tr> <th>Turma</th> <th>Número</th> <th>Nome</th> <th>Alg</th> <th>Java</th> <th>VB</th> <th>Final</th> </tr>";
 
     for (int i = 0; i < nElems; i++) {
       tabela += "<tr> <td>"+ turmas[i].trim() +"</td> <td>"+ numeros[i] +"</td> <td>"+ nomes[i].trim() +"</td> <td>"+ algNotas[i] +"</td> <td>"+ javaNotas[i] +"</td> <td>"+ vbNotas[i] +"</td> <td>"+ notaFinal(algNotas[i], javaNotas[i], vbNotas[i], 0.30, 0.40, 0.30) +"</td> </tr>";
@@ -222,4 +251,47 @@ public class Main {
 
   }
 
+  private static int checkNumber(String input, String nome) {
+
+    int output = -1;
+
+    while (output == -1){
+      try {
+        output = Integer.parseInt(input);
+      }catch (NumberFormatException e){
+        JOptionPane.showMessageDialog(null, "Numero inválido!", "Falha ao inserir numero", JOptionPane.WARNING_MESSAGE);
+
+        input = JOptionPane.showInputDialog(null, "Insira o numero do(a) aluno(a) " + nome + ":", "", JOptionPane.PLAIN_MESSAGE);
+      }
+    }
+
+    return output;
+
+  }
+
+  private static int checkNota(String input, String nome, String notaNome) {
+
+    int output = -1;
+
+    while (output == -1){
+      try {
+        output = Integer.parseInt(input);
+      }catch (NumberFormatException e){
+        JOptionPane.showMessageDialog(null, "Nota inválida!", "Falha ao inserir nota", JOptionPane.WARNING_MESSAGE);
+
+        input = JOptionPane.showInputDialog(null, "Insira a nota de "+ notaNome +" do(a) aluno(a) " + nome + ":", "", JOptionPane.PLAIN_MESSAGE);
+      }
+
+      if(output < 0 || output > 20){
+        output = -1;
+
+        JOptionPane.showMessageDialog(null, "Nota inválida!", "Falha ao inserir nota", JOptionPane.WARNING_MESSAGE);
+        input = JOptionPane.showInputDialog(null, "Insira a nota de "+ notaNome +" do(a) aluno(a) " + nome + ":", "", JOptionPane.PLAIN_MESSAGE);
+      }
+
+    }
+
+    return output;
+
+  }
 }
